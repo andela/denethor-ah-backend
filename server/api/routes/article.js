a4b35fb3d2007192e3ca5ab789f28e859f80e3ed
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-01-28 15:29:59 +0100

    feat(highlightArticle): implement feature for highlighting and commenting on an Article - Create model to store highlight and comment - Implement endpoint to create a highlight and comment entry - Implement endpoint to fetch highlight and commit entry for an Article - Test feature for specificied output [Delivers #163478818]

--- server/api/routes/article.js
+++ server/api/routes/article.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import passport from 'passport';
-import { newArticleValidator } from '../middlewares/validation/article';
-import { createArticle, likeArticle, dislikeArticle } from '../controllers/article';
+import { newArticleValidator, createHighlightValidator } from '../middlewares/validation/article';
+import {
+  createArticle, likeArticle, dislikeArticle, createHighlight, getHighlights
+} from '../controllers/article';
 import commentsRouter from './comment';
 
 const articlesRouter = Router();
@@ -9,6 +11,12 @@ const articlesRouter = Router();
 articlesRouter.post('/', passport.authenticate('jwt', { session: false }), newArticleValidator, createArticle);
 articlesRouter.patch('/:id/likes', passport.authenticate('jwt', { session: false }), likeArticle);
 articlesRouter.patch('/:id/dislikes', passport.authenticate('jwt', { session: false }), dislikeArticle);
+articlesRouter.post('/:id/highlights',
+  passport.authenticate('jwt', { session: false }),
+  createHighlightValidator,
+  createHighlight);
+articlesRouter.get('/:id/highlights', passport.authenticate('jwt', { session: false }), getHighlights);
+
 articlesRouter.use('/:articleId/comments', commentsRouter);
 
 export default articlesRouter;
