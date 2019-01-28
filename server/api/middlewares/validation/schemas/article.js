695fcb9debeeb121507dfb3f17c8efe26a865aea
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-01-28 15:29:59 +0100

    feat(highlightArticle): implement feature for highlighting and commenting on an Article - Create model to store highlight and comment - Implement endpoint to create a highlight and comment entry - Implement endpoint to fetch highlight and commit entry for an Article - Test feature for specificied output [Delivers #163478818]

--- server/api/middlewares/validation/schemas/article.js
+++ server/api/middlewares/validation/schemas/article.js
@@ -3,17 +3,19 @@ import Joi from 'joi';
 const slug = Joi.string().min(5).required();
 const description = Joi.string().min(10).required();
 const body = Joi.string().min(20).required();
-const authorId = Joi.string();
 const references = Joi.array().items(Joi.string());
 const categoryId = Joi.number().required();
 const tags = Joi.string();
+const highlight = Joi.string().trim().min(1).required();
+const comment = Joi.string().trim().min(1).required();
 
 export const newArticleSchema = {
   slug,
   description,
   body,
-  authorId,
   references,
   categoryId,
   tags
 };
+
+export const createHighlightSchema = { highlight, comment };
---
+++ server/models/highlightcomment.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const HighlightComment = sequelize.define('HighlightComment', {
+    highlight: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    comment: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    articleId: DataTypes.UUID,
+    readerId: DataTypes.UUID
+  });
+  return HighlightComment;
+};
