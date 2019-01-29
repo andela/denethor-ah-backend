76affa67c890625229ac8953611af3bb1d4e32f4
Peace Oyedeji <peaceoyedeji@Peaces-MacBook-Pro.local>
2019-01-29 16:43:42 +0100

    chore:Setup express server and Babel - Install express - Setup server to run on port 3000 - Install Babel -Add .babelrc file
    [Delivers #163559030]

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import express from 'express';
 import bodyParser from 'body-parser';
 import session from 'express-session';
@@ -11,6 +12,22 @@ const logger = createLogger({
   format: format.simple(),
   transports: [new transports.Console()]
 });
+=======
+const fs = require("fs"),
+    http = require("http"),
+    path = require("path"),
+    methods = require("methods"),
+    express = require("express"),
+    bodyParser = require("body-parser"),
+    session = require("express-session"),
+    cors = require("cors"),
+    passport = require("passport"),
+    errorhandler = require("errorhandler"),
+    mongoose = require("mongoose");
+    import dotenv from 'dotenv';
+
+dotenv.config();
+>>>>>>> chore:Setup express server and Babel
 
 dotenv.config();
 
@@ -39,6 +56,7 @@ app.use(
   })
 );
 
+<<<<<<< HEAD
 app.get('/', (req, res) => res.status(200).send({
   status: 'connection successful',
   message: 'Welcome to Author Haven!',
@@ -49,3 +67,70 @@ app.listen(port, () => {
 });
 
 export default app;
+=======
+// if (!isProduction) {
+//     app.use(errorhandler());
+// }
+
+// if (isProduction) {
+//     mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//     mongoose.connect("mongodb://localhost/conduit");
+//     mongoose.set("debug", true);
+// }
+
+
+require("./models/User");
+
+app.use(require("./routes"));
+
+app.get('/', (req, res) => res.status(200).send({
+    status: 'connection successful',
+    message: 'Welcome to Author Haven!',
+  }));
+  
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    console.log('yea');
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+// if (!isProduction) {
+//     app.use(function(err, req, res, next) {
+//         console.log(err.stack);
+
+//         res.status(err.status || 500);
+
+//         res.json({
+//             errors: {
+//                 message: err.message,
+//                 error: err
+//             }
+//         });
+//     });
+// }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    console.log('yeah naby');
+    res.status(err.status || 500);
+    res.json({
+        errors: {
+            message: err.message,
+            error: {}
+        }
+    });
+});
+
+// finally, let's start our server...
+const server = app.listen(process.env.PORT || 3000, function() {
+    console.log("Listening on port " + server.address().port);
+});
+>>>>>>> chore:Setup express server and Babel
